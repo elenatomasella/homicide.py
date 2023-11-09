392981b1779b2bdac609579e8dd4ae4c386efd0e
elenatomasella <149505151+elenatomasella@users.noreply.github.com>
2023-11-09 17:00:29 +0100

    EDA start after data cleaning.py

--- homicide.py
+++ homicide.py
@@ -20,3 +20,81 @@ import matplotlib.pyplot as plt
 
 #check for na values in the dataframe
 print(homicide_df.isnull().values.any())
+
+#view the clean dataset -partially
+homicide_df.describe()
+
+#continue data-cleaning
+#for now we have two numerical variables only so we need to convert the other ones properly
+homicide_df.columns
+set(homicide_df['Crime Type'])
+#{'Manslaughter by Negligence', 'Murder or Manslaughter'}
+homicide_df['Crime Type'] = homicide_df['Crime Type'].replace({'Murder or Manslaughter': 1, 'Manslaughter by Negligence': 0})
+homicide_df = homicide_df.rename(columns={'Crime Type': 'Crime Type: intentional manslaughter?'})
+
+set(homicide_df['Crime Solved'])
+#convertions into a dummy variable
+homicide_df['Crime Solved'] = homicide_df['Crime Solved'].replace({'Yes': 1, 'No': 0})
+
+set(homicide_df['Victim Sex'])
+
+set(homicide_df['Victim Age'])
+#here we have outliers (age 998) !!
+homicide_df = homicide_df[homicide_df['Victim Age']!= 998]
+
+set(homicide_California['Perpetrator Sex'])
+set(homicide_California['Perpetrator Age'])
+set(homicide_California['Perpetrator Race'])
+set(homicide_California['Perpetrator Ethnicity'])
+#{'Asian/Pacific Islander', 'White', 'Black', 'Native American/Alaska Native', 'Unknown'}
+set(homicide_California['Weapon'])
+#{'Asian/Pacific Islander', 'White', 'Black', 'Native American/Alaska Native', 'Unknown'}
+
+#EDA
+# Victim race distribution overall
+victim_race_counts = homicide_df['Victim Race'].value_counts()
+plt.pie(victim_race_counts)
+plt.title('Victim Race overall distribution')
+plt.legend(title = 'Races', loc = 'upper center', labels = set(homicide_df['Victim Race']))
+plt.show()
+#victim race distribution in California
+victim_race_counts_California = homicide_California['Victim Race'].value_counts()
+plt.pie(victim_race_counts)
+plt.title('Victim Race in California')
+plt.legend(title = 'Races', loc = 'upper center', labels = set(homicide_California['Victim Race']))
+plt.show()
+#victim race distribution in Texas
+victim_race_counts_Texas = homicide_Texas['Victim Race'].value_counts()
+plt.pie(victim_race_counts)
+plt.title('Victim Race in Texas')
+plt.legend(title = 'Races', loc = 'upper center', labels = set(homicide_Texas['Victim Race']))
+plt.show()
+
+#distribution od crimes through the years: comparison between California and Texas
+sns.histplot(homicide_df, x = 'Year', hue = 'State', bins = 35,  multiple='dodge', shrink=.75)
+plt.figure(figsize=(8, 6))
+plt.show()
+
+#distribution od crimes through the years: comparison between capitals
+sns.histplot(homicide_df[(homicide_df['City']=='Austin') | (homicide_df['City']=='Sacramento')], x = 'Year', hue = 'City', bins = 30,  multiple='dodge', shrink=.75)
+plt.figure(figsize=(8, 6))
+plt.show()
+
+#percentage of solved crimes through the years
+solved_crimes = []
+for i in list(set(homicide_df['Year'])):
+    solved_crimes.append(len(homicide_df[(homicide_df['Year']==i) &(homicide_df['Crime Solved']==1)])/len(homicide_df[homicide_df['Year']==i]))
+solved_crimes_cal = []
+for i in list(set(homicide_California['Year'])):
+    solved_crimes_cal.append(len(homicide_California
+                             [(homicide_California
+                                          ['Year']==i) &(homicide_California['Crime Solved']=='Yes')])/len(homicide_California[homicide_California['Year']==i]))
+solved_crimes_tex = []
+for i in list(set(homicide_Texas['Year'])):
+    solved_crimes_tex.append(len(homicide_Texas
+                             [(homicide_Texas
+                                          ['Year']==i) &(homicide_Texas['Crime Solved']=='Yes')])/len(homicide_Texas[homicide_Texas['Year']==i]))
+plt.plot(range(1980,2015), solved_crimes, color = 'blue')
+plt.plot(range(1980,2015), solved_crimes_cal, color = 'red')
+plt.plot(range(1980,2015), solved_crimes_tex, color = 'green')
+plt.show()
