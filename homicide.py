fce9bdda5d5f0c4f70af5387d6cc5ae78ca555d8
elenatomasella <149505151+elenatomasella@users.noreply.github.com>
2023-11-12 10:10:42 +0100

    customized plots.py
    last code is still to be fit

--- homicide.py
+++ homicide.py
@@ -35,6 +35,8 @@ homicide_df = homicide_df.rename(columns={'Crime Type': 'Crime Type: intentional
 
 set(homicide_df['Crime Solved'])
 homicide_df['Crime Solved'] = homicide_df['Crime Solved'].replace({'Yes': 1, 'No': 0})
+homicide_California['Crime Solved'] = homicide_California['Crime Solved'].replace({'Yes': 1, 'No': 0})
+homicide_Texas['Crime Solved'] = homicide_Texas['Crime Solved'].replace({'Yes': 1, 'No': 0})
 #it's not necessary to change the type to interepret the regression eventually
 
 set(homicide_df['Victim Sex'])
@@ -43,12 +45,23 @@ set(homicide_df['Victim Sex'])
 set(homicide_df['Victim Age'])
 #here we have outliers: (age 998) !! We then decided to delete that row
 homicide_df = homicide_df[homicide_df['Victim Age']!= 998]
+plt.figure(figsize=(8, 6))
+homicide_df_sorted = homicide_df.sort_values(by=['Victim Age'])
+sns.lineplot(data=homicide_df, x='Year', y='Victim Age', hue='State', palette=['orchid', 'navy'], marker='x', linestyle = '--')
+plt.title('Victim Age trend')
+plt.show()
 
 set(homicide_California['Perpetrator Sex'])
 #for now we do not convert into dummies as we want to keep this particular variable as categorical for the EDA
 
 set(homicide_California['Perpetrator Age'])
 #outliers 95/05 : per Edo
+homicide_df = homicide_df[homicide_df['Perpetrator Age']!= 0]
+plt.figure(figsize=(8, 6))
+homicide_df_sorted = homicide_df.sort_values(by=['Perpetrator Age'])
+sns.lineplot(data=homicide_df, x='Year', y='Perpetrator Age', hue='State', palette=['orchid', 'navy'], marker='o')
+plt.title('Perpetrator Age trend')
+plt.show()
 
 set(homicide_California['Perpetrator Race'])
 set(homicide_California['Perpetrator Ethnicity'])
@@ -61,7 +74,6 @@ set(homicide_California['Weapon'])
 #EDA
 #population in Texas and California: extention of the research
 population = pd.read_csv('population_usafacts.csv')
-type(population)
 #population US
 pop = []
 for i in range(1980,2015):
@@ -75,65 +87,74 @@ pop_tex = []
 for i in range(81, 116):
     pop_tex.append(population.iloc[64, i])
 #trend of the population in Texas and California from 2020 to 2014
-plt.plot(range(2000,2015), pop_tex[20:],color = 'blue', label = 'Texas')
-plt.plot(range(2000, 2015), pop_cal[20:], color = 'seagreen', label = 'California ')
+plt.plot(range(2000,2015), pop_tex[20:],color = 'navy',linestyle='--' , label = 'Texas')
+plt.plot(range(2000, 2015), pop_cal[20:], color = 'orchid', linestyle ="--" ,label = 'California ')
 plt.title('Population Over Years')
 plt.xlabel('Year')
 plt.ylabel('Population')
 plt.legend()
 plt.show()
 
+#% of homocides in capitals
+len(homicide_Sacramento)/len(homicide_California)
+len(homicide_Austin)/len(homicide_Texas)
+
+#EDA about victim race
 # Victim race distribution overall
+labels = set(homicide_California['Victim Race'])
+colors = ['dimgrey', 'brown', 'snow', 'burlywood', 'turquoise']
 import matplotlib.pyplot as plt
 victim_race_counts = homicide_df['Victim Race'].value_counts()
-plt.pie(victim_race_counts)
+plt.pie(victim_race_counts, colors = colors, shadow = True, startangle = 140 )
 plt.title('Victim Race overall distribution')
-plt.legend(title = 'Races', loc = 'upper center', labels = set(homicide_df['Victim Race']))
+plt.legend(title = 'Races', loc = 'lower center',labels = labels)
 plt.show()
 #victim race distribution in California
 victim_race_counts_California = homicide_California['Victim Race'].value_counts()
-plt.pie(victim_race_counts_California)
+plt.pie(victim_race_counts_California, colors=colors, shadow=True, startangle=140)
 plt.title('Victim Race in California')
-plt.legend(title = 'Races', loc = 'upper center', labels = set(homicide_California['Victim Race']))
+plt.legend(title = 'Races', loc = 'lower center', labels = labels)
 plt.show()
 #victim race distribution in Texas
 victim_race_counts_Texas = homicide_Texas['Victim Race'].value_counts()
-plt.pie(victim_race_counts_Texas)
+plt.pie(victim_race_counts_Texas, colors = colors, shadow = True, startangle=140)
 plt.title('Victim Race in Texas')
-plt.legend(title = 'Races', loc = 'upper center', labels = set(homicide_Texas['Victim Race']))
+plt.legend(title = 'Races', loc = 'lower center',labels = labels)
 plt.show()
 
 #EDA about perpetrator race
 # Perpetrator race distribution overall
-import matplotlib.pyplot as plt
 perpetrator_race_counts = homicide_df['Perpetrator Race'].value_counts()
-plt.pie(perpetrator_race_counts)
+plt.pie(perpetrator_race_counts, colors = colors)
 plt.title('Perpetrator Race overall distribution')
-plt.legend(title = 'Races', loc = 'upper center', labels = set(homicide_df['Perpetrator Race']))
+plt.legend(title = 'Races', loc = 'upper center', labels =labels)
 plt.show()
 #perpetrator race distribution in California
 perpetrator_race_counts_cal = homicide_California['Perpetrator Race'].value_counts()
-plt.pie(perpetrator_race_counts_cal)
+plt.pie(perpetrator_race_counts_cal, colors = colors)
 plt.title('Perpetrator Race in California')
-plt.legend(title = 'Races', loc = 'upper center', labels = set(homicide_df['Perpetrator Race']))
+plt.legend(title = 'Races', loc = 'upper center', labels = labels)
 plt.show()
-#victim race distribution in Texas
+#perpetrator race distribution in Texas
 perpetrator_race_counts_tex = homicide_Texas['Perpetrator Race'].value_counts()
-plt.pie(perpetrator_race_counts_tex)
+plt.pie(perpetrator_race_counts_tex, colors = colors)
 plt.title('Perpetrator Race in Texas')
-plt.legend(title = 'Races', loc = 'upper center', labels = set(homicide_Texas['Perpetrator Race']))
-plt.show()
-#distribution od crimes through the years: comparison between California and Texas
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.histplot(homicide_df, x = 'Year', hue = 'State', bins = 35,  multiple='dodge', shrink=.75)
-plt.figure(figsize=(8, 6))
+plt.legend(title = 'Races', loc = 'upper center', labels = labels)
 plt.show()
 
-#distribution od crimes through the years: comparison between capitals
-sns.histplot(homicide_df[(homicide_df['City']=='Austin') | (homicide_df['City']=='Sacramento')], x = 'Year', hue = 'City', bins = 30,  multiple='dodge', shrink=.75)
+
+#distribution of crimes through the years: comparison between California and Texas
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.histplot(homicide_df, x = 'Year', hue = 'State', bins = 35,  multiple='dodge', shrink=.75, palette = ['orchid','navy'])
 plt.figure(figsize=(8, 6))
 plt.show()
+#distribution of crimes through the years: comparison between capitals
+sns.histplot(homicide_df[(homicide_df['City']=='Austin') | (homicide_df['City']=='Sacramento')], x = 'Year', hue = 'City', bins = 30,  multiple='dodge', shrink=.75)
+plt.figure(figsize=(8, 6))
+plt.title('Crimes count')
+plt.show()
+
 
 #percentage of solved crimes through the years
 solved_crimes = []
@@ -143,19 +164,27 @@ solved_crimes_cal = []
 for i in list(set(homicide_California['Year'])):
     solved_crimes_cal.append(len(homicide_California
                              [(homicide_California
-                                          ['Year']==i) &(homicide_California['Crime Solved']=='Yes')])/len(homicide_California[homicide_California['Year']==i]))
+                                          ['Year']==i) &(homicide_California['Crime Solved']==1)])/len(homicide_California[homicide_California['Year']==i]))
 solved_crimes_tex = []
 for i in list(set(homicide_Texas['Year'])):
     solved_crimes_tex.append(len(homicide_Texas
                              [(homicide_Texas
-                                          ['Year']==i) &(homicide_Texas['Crime Solved']=='Yes')])/len(homicide_Texas[homicide_Texas['Year']==i]))
-import matplotlib.pyplot as plt
-plt.plot(range(1980,2015), solved_crimes, color = 'blue', label = 'generic')
-plt.plot(range(1980,2015), solved_crimes_cal, color = 'red',label = 'California' )
-plt.plot(range(1980,2015), solved_crimes_tex, color = 'green', label = 'Texas')
+                                          ['Year']==i) &(homicide_Texas['Crime Solved']==1)])/len(homicide_Texas[homicide_Texas['Year']==i]))
+plt.plot(range(1980,2015), solved_crimes, color = 'peachpuff', label = 'generic')
+plt.plot(range(1980,2015), solved_crimes_cal, color = 'orchid',label = 'California' )
+plt.plot(range(1980,2015), solved_crimes_tex, color = 'navy', label = 'Texas')
 plt.legend()
 plt.show()
 
-#population analysis
-len(homicide_Sacramento)/len(homicide_California)
-len(homicide_Austin)/len(homicide_Texas)
+
+#DATA ANALYSIS
+#does the perpetrator age affect the amount on unsolved crimes? NOT WORKING HERE!!
+plt.figure(figsize=(8, 6))
+homicide_df = homicide_df[homicide_df['Perpetrator Age']!=0]
+homicide_df.groupby('Perpetrator Age')
+sns.countplot(x='Perpetrator Age', hue='Crime Solved', data=homicide_df, palette=['lightgreen', 'salmon'])
+plt.title('Distribution of Solved and Unsolved Crimes by Perpetrator Age')
+plt.xlabel('Perpetrator Age')
+plt.ylabel('Count')
+plt.legend(['solved', 'unsolved'])
+plt.show()
